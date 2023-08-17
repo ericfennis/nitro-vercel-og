c0222739717fe013e7f6dc953f7ceaeaa0dbd38b
Eric Fennis <eric.fennis@gmail.com>
2023-08-17 15:43:20 +0200

    update config

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,9 +1,12 @@
+import { wasm } from '@rollup/plugin-wasm';
+import copy from 'rollup-plugin-copy'
+
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   preset: 'vercel-edge',
-  experimental: {
-    wasm: true,
-  },
+  // experimental: {
+  //   wasm: true,
+  // },
   esbuild: {
     options: {
       // This was needed to get React ro work
@@ -11,4 +14,27 @@ export default defineNitroConfig({
       jsxFragment: 'React.Fragment',
     },
   },
+  rollupConfig: {
+    // extner
+    external: [
+      './yoga.wasm?module',
+      './resvg.wasm?module',
+    ],
+    plugins: [
+      copy({
+        targets: [
+          { src: './node_modules/@vercel/og/dist/yoga.wasm', dest: './.vercel/output/functions/__nitro.func' },
+          { src: './node_modules/@vercel/og/dist/resvg.wasm', dest: './.vercel/output/functions/__nitro.func' },
+        ]
+      })
+      // wasm({
+      //   sync: [
+      //     '@vercel/og/dist/yoga.wasm?module',
+      //     '@vercel/og/dist/resvg.wasm?module',
+      //     // 'web/foobar.wasm'
+      //   ]
+      // }),
+
+    ]
+  }
 })
